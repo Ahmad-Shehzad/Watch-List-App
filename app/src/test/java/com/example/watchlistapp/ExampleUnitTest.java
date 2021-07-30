1d0e86043c804061176e08676f287909fa3c0b81
Samuel Stacey <ss3467@bath.ac.uk>
2021-07-31 00:25:46 +0100

    -Updated unit tests to follow test driven development

--- app/src/test/java/com/example/watchlistapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/watchlistapp/ExampleUnitTest.java
@@ -10,6 +10,14 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Test
+    public void isAhmadBad() {
+        final String Ahmad = "bad";
+
+        assertEquals(Ahmad.toString(), "bad");
+    }
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
